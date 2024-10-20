236791f94dcfc81d4bf7acfbd529a8721cfc56bc
mrcoder57 <tiwariji2300@gmail.com>
2024-10-20 16:48:39 +0530

    feature:add footer and toady

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Education from '@/components/education/education'
 import Experience from '@/components/experience/experience'
 import Faisandu from '@/components/faisandu/faisandu'
+import Footer from '@/components/footer/footer'
 import Santander from '@/components/santander/santander'
 import Skills from '@/components/skills/skills'
 import SuraComp from '@/components/sura/sura'
+import Today from '@/components/today/today'
 import Welcome from '@/components/welcome/welcome'
 import WorkExperience from '@/components/workExperience/workExperience'
 
@@ -35,6 +37,12 @@ const Home = () => {
       </div>
       <div className=' h-full flex items-center max-w-6xl w-full justify-center'>
       <SuraComp/>
+      </div>
+      <div className=' h-full flex items-center max-w-6xl w-full justify-center'>
+      <Today/>
+      </div>
+      <div className=' h-full flex items-center max-w-6xl w-full justify-center'>
+      <Footer/>
       </div>
     </div>
   )
