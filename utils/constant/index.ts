04044faf2825c2a6575b52654d01d98c9a2eef35
mrcoder57 <tiwariji2300@gmail.com>
2024-10-20 16:04:10 +0530

    create constant

---
+++ utils/constant/index.ts
@@ -0,0 +1,13 @@
+export const skills=[
+    {name:"React"},
+    {name:"Next.js"},
+    {name:"Tailwind"},
+    {name:"Typescript"},
+    {name:"HTML"},
+    {name:"CSS"},
+    {name:"Docker"},
+    {name:"Graphql"},
+    {name:"Express js"},
+    {name:"Node js"},
+    {name:"AWS"},     
+]
