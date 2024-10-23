59bea18a3c7423c6f79cfacc5d2ce1fc2dd74898
mrcoder57 <tiwariji2300@gmail.com>
2024-10-23 19:51:18 +0530

    fix: make welcome responsive

--- components/welcome/welcome.tsx
+++ components/welcome/welcome.tsx
@@ -5,13 +5,20 @@ import React from "react";
 const Welcome = () => {
   return (
     <div className=" w-full flex flex-col items-center justify-center my-auto h-full">
-      <div className="flex items-center justify-center">
-        <Image src={"/Avatar.png"} alt="portfolio" width={280} height={280} />
+      <div className="flex items-center justify-center lg:w-[280px] lg:h-[280px]  w-[100px] h-[100px]">
+        <Image
+          src="/Avatar.png"
+          alt="portfolio"
+          layout="responsive" // Ensure the image maintains its aspect ratio
+          width={280} // Original width of the image
+          height={280} // Original height of the image
+          className="object-cover" // Ensures the image covers the div without distortion
+        />
       </div>
       <div className="flex items-center justify-center text-center">
         {" "}
         <p
-          className={`${space_Mono.className} bg-gradient-to-r from-[#DBA6F4] to-[#A981FE] bg-clip-text text-transparent text-[96px] font-bold `}
+          className={`${space_Mono.className} bg-gradient-to-r from-[#DBA6F4] to-[#A981FE] bg-clip-text text-transparent lg:text-[96px] text-[24px] font-bold `}
         >
           Hi. I'm Aman. A Fullstack developer.
         </p>
